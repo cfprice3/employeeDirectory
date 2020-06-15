1f449b76b15f92caad9ce7a12c60d3354b10204f
cfprice3 <cfprice3@gmail.com>
2020-06-15 16:11:39 -0400

    did some pseudo coding, created utils folder with api call to randomuser database.

--- app.js
+++ app.js
@@ -7,3 +7,16 @@ const port = process.env.PORT || 3000
 app.use(express.static(path.join(__dirname, 'root')));
 
 app.listen(port, () => console.log(`Connected on PORT: ${port}`))
+
+
+
+//make a utils folder, api.js with ajax call
+
+//
+//ajax call needed
+// export default {
+//     // Gets all users
+//     getUsers: function() {
+//       return axios.get("https://randomuser.me/api/?results=200&nat=us");
+//     }
+//   };
