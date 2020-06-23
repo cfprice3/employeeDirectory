0ab4b90d0a24cc3a6f828f71302582eaaf5d9895
cfprice3 <cfprice3@gmail.com>
2020-06-23 16:52:40 -0400

    creating component for body of employee table

--- src/components/BodyData.js
+++ src/components/BodyData.js
@@ -4,6 +4,37 @@ import DataAreaContext from "../utils/DataAreaContext";
 
 const BodyData = () => {
     const context = useContext(DataAreaContext);
+
+    return (
+        // The <tbody> element is used to specify the body part of a table
+        <tbody>
+            {context.developerState.filteredUsers[0] !== undefined && context.developerState.filteredUsers[0].name !== undefined ? (
+                context.developerState.filteredUsers.map(({ login, name, picture, phone, email }) => (
+                    <tr key={login.uuid}>
+                        <td data-th="Image" className="align-middle">
+                            <img
+                                src={picture.medium}
+                                alt={"profile image for " + name.first + " " + name.last}
+                                className="img-responsive" />
+                        </td>
+                        <td data-th="Name" className="name-cell align-middle">
+                            {name.first} {name.last}
+                        </td>
+                        <td data-th="Phone" className="align-middle">
+                            {phone}
+                        </td>
+                        <td data-th="Email" className="align-middle">
+                            <a href={"mailto:" + email} target="__blank">
+                                {email}
+                            </a>
+                        </td>
+                    </tr>
+                ))
+            ) : (
+                <></>
+            )}
+            </tbody>
+        );
 }
 
 export default BodyData;
--- src/components/empData.js
+++ src/components/empData.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import API from "../utils/API";
 
+
 const empData = () => {
     const [developerState, setDeveloperState] = useState({
       users: [],
