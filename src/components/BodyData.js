206007776d66de44de264c54dda5ffb4c4d6f286
cfprice3 <cfprice3@gmail.com>
2020-06-23 16:21:13 -0400

    creating component for body

---
+++ src/components/BodyData.js
@@ -0,0 +1,9 @@
+
+import React, { useContext } from "react";
+import DataAreaContext from "../utils/DataAreaContext";
+
+const BodyData = () => {
+    const context = useContext(DataAreaContext);
+}
+
+export default BodyData;
